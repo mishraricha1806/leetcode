f9ee2182e01c5fcc0daefa86cc30758c13acd083
mishraricha1806 <71933219+mishraricha1806@users.noreply.github.com>
2022-12-18 00:54:53 +0530

    Update ContainsDuplicate.java

--- ContainsDuplicate.java
+++ ContainsDuplicate.java
@@ -1,1 +1,12 @@
-HI
+class ContainsDuplicate {
+    public boolean containsDuplicates(int[] nums) {
+        HashSet set=new HashSet();
+        for(int i:nums){
+            if(set.contains(i)){
+                return true;
+            }
+            set.add(i);
+        }
+        return false;
+    }
+}
